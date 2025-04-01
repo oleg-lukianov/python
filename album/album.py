90b8bdb26cb4f3f09620b62fb95bdc7285009e57
Oleg Lukianov <oleg.lukianov@raiffeisen.ua>
2025-04-01 21:10:58 +0300

    Album is finished

--- album/album.py
+++ album/album.py
@@ -1,60 +1,57 @@
-import time
-from flask import Flask, render_template
+"""
+Album on Flask engine
+"""
 import os
-from multiprocessing import Process, Value
+import re
 import random
+from flask import Flask, render_template
 
-PEOPLE_FOLDER = os.path.join('static', 'people_photo')
+FOLDER = os.path.join('static', 'photo')
 ALL_PHOTO = []
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
-full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'IMG_20210129_193309.jpg')
+app.config['UPLOAD_FOLDER'] = FOLDER
+full_filename = None
 
 @app.route('/')
 @app.route('/index')
+@app.route('/album')
 def show_index():
-#    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'IMG_20210129_193309.jpg')
+    """
+    Show index
+    """
     full_filename = get_random_jpg()
     return render_template("index.html", user_image = full_filename)
 
 
-
-def record_loop(loop_on):
-   while True:
-      if loop_on.value == True:
-          print("full_filename: ", full_filename)
-      time.sleep(1)
-
-
 def find_jpg():
-    path_main = "/home/ALL/github/python/album/static/Nastya_young_Album_1"
-    path_web = "static/Nastya_young_Album_1"
+    """
+    Find jpg
+    """
+    path_main = "static/photo"
     for root, _, files in os.walk(path_main):
-            for fname in files:
-                filename = os.path.join(path_web, fname)
+        for fname in files:
+            if re.search("favicon.png", fname):
+                pass
+            else:
+                filename = os.path.join(path_main, fname)
                 ALL_PHOTO.append(filename)
- #               print(f'filename: {filename}')
-  #              time.sleep(1)
+                print(f'filename: {filename}')
 
 
 def get_random_jpg():
-    random_photo = random.randrange(0, len(ALL_PHOTO))
-#    for x in ALL_PHOTO:
- #       print("path: ", x, " count: ", len(ALL_PHOTO), " rand: ", random_photo)
-    #print("photo: ", ALL_PHOTO[random_photo])
- #   return render_template("index.html", user_image = full_filename)
-    return ALL_PHOTO[random_photo]
+    """
+    Get random jpg
+    """
+    if len(ALL_PHOTO) == 0:
+        full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'favicon.png')
+        print("All_PHOTO: ", len(ALL_PHOTO))
+        return full_filename
+    else:
+        random_photo = random.randrange(0, len(ALL_PHOTO))
+        return ALL_PHOTO[random_photo]
 
 
 if __name__ == "__main__":
     find_jpg()
- #   recording_on = Value('b', True)
-  #  p = Process(target=find_jpg)
-   # p.start()
-    app.run(host='0.0.0.0', port=10052, debug=True)
-    #p.join()
-
-
-
-
+    app.run(host='0.0.0.0', port=8443, debug=True)

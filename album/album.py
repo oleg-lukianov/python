79e1c63328ea06fc6bf4f282ff86346abbb2d66b
Oleg Lukianov <oleg.lukianov@raiffeisen.ua>
2025-04-02 13:01:22 +0300

    fix album

--- album/album.py
+++ album/album.py
@@ -7,11 +7,12 @@ import random
 from flask import Flask, render_template
 
 FOLDER = os.path.join('static', 'photo')
+PATH_MAIN = os.path.join(os.path.dirname(__file__), 'static', 'photo')
 ALL_PHOTO = []
+full_photo_name = None
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = FOLDER
-full_filename = None
 
 @app.route('/')
 @app.route('/index')
@@ -20,23 +21,24 @@ def show_index():
     """
     Show index
     """
-    full_filename = get_random_jpg()
-    return render_template("index.html", user_image = full_filename)
+    full_photo_name = get_random_jpg()
+    return render_template("index.html", user_image = full_photo_name)
 
 
 def find_jpg():
     """
     Find jpg
     """
-    path_main = "static/photo"
-    for root, _, files in os.walk(path_main):
+    print("Search PATH_MAIN: ", PATH_MAIN)
+    for root, _, files in os.walk(PATH_MAIN):
         for fname in files:
+            # print("fname: ", fname)
             if re.search("favicon.png", fname):
                 pass
             else:
-                filename = os.path.join(path_main, fname)
-                ALL_PHOTO.append(filename)
-                print(f'filename: {filename}')
+                full_photo_name = os.path.join(FOLDER, fname)
+                ALL_PHOTO.append(full_photo_name)
+                # print(f'full_photo_name: {full_photo_name}')
 
 
 def get_random_jpg():
@@ -44,12 +46,15 @@ def get_random_jpg():
     Get random jpg
     """
     if len(ALL_PHOTO) == 0:
-        full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'favicon.png')
-        print("All_PHOTO: ", len(ALL_PHOTO))
-        return full_filename
+        full_photo_name = os.path.join(app.config['UPLOAD_FOLDER'], 'favicon.png')
+        random_photo = 0
+        ALL_PHOTO.append(full_photo_name)
     else:
         random_photo = random.randrange(0, len(ALL_PHOTO))
-        return ALL_PHOTO[random_photo]
+
+    print("Count All_PHOTO: ", len(ALL_PHOTO))
+
+    return ALL_PHOTO[random_photo]
 
 
 if __name__ == "__main__":
